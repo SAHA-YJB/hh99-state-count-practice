471f08b43a0054f5f0d3db67845165d8af1312dc
SAHA-YJB <pkomom123@naver.com>
2023-06-23 20:50:58 +0900

    state-onclick 관계

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const onClickPlus = () => {
+    setCount(count + 1);
+    if(count === 5) {
+      alert('그만눌러')
+    }
+  };
+  const onClickMinus = () => {
+    setCount(count - 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>{count}</div>
+      <div>
+        <button onClick={onClickPlus}>+1</button>
+        <button onClick={onClickMinus}>-1</button>
+      </div>
     </div>
   );
 }
